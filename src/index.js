7c5a737c9934681290df9e8e767f46c5e02cf8f6
Leovita777 <141558126+Leovita777@users.noreply.github.com>
2025-06-10 23:42:01 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// // src/index.js
+
+// import express from 'express';
+
+// const app = express();
+
+// const PORT = 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+console.log('Приложение запущено и работает!');
